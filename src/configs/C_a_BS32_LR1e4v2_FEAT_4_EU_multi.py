c65a774b3685a2f388f72dc9ca8ba8323ac17f31
Frankalexej <tt115889@connect.hku.hk>
2025-11-05 15:01:02 +0800

    20251105 + 1

--- src/configs/C_a_BS32_LR1e4v2_FEAT_4_EU_multi.py
+++ src/configs/C_a_BS32_LR1e4v2_FEAT_4_EU_multi.py
@@ -1,8 +1,9 @@
 # Configuration parameters
 BATCH_SIZE = 32
-EPOCHS = 200
+EPOCHS = 150
 LR = 1e-4
 READ_BASE_PATH = '/mnt/storage/ldl_linguistics/PhonGen2025'
+MODEL_LOAD_BASE_PATH = '/mnt/storage/franklhtan/projects/PhonGen2025'
 WRITE_BASE_PATH = '/mnt/storage/franklhtan/projects/PhonGen2025'
 NPY_BASE_PATH = '/mnt/storage/ldl_linguistics'
 CSV_PATH = '/mnt/storage/ldl_linguistics/PhonGen2025/data_251011/data_train_phase1/metadata_train_phase1.csv'
@@ -14,6 +15,8 @@ OUT_FEATURES = 4
 TEMPERATURE = 0.07
 MINMAX = False
 # RUN_NAME = 'a_BS32_LR1E4_FEAT4_TEMP007V2_EU'
+RUN_TIMES_START = 1
+RUN_TIMES_END = 10
 RUN_NAMES = ['a_BS32_LR1E4_FEAT4_TEMP007V2_EU_1AT50_both',
-             'a_BS32_LR1E4_FEAT4_TEMP007V2_EU_1phaseONLY',
-             'a_BS32_LR1E4_FEAT4_TEMP007V2_EU_2AT50_both']
+             'a_BS32_LR1E4_FEAT4_TEMP007V2_EU_1phaseONLY',]
+            #  'a_BS32_LR1E4_FEAT4_TEMP007V2_EU_2AT50_both']
