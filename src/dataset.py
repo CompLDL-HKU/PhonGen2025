07a77e9d6d01886420be259d409848b9a749ecb2
Frankalexej <tt115889@connect.hku.hk>
2026-01-06 16:15:28 +0800

    20260106

--- src/dataset.py
+++ src/dataset.py
@@ -162,3 +162,47 @@ class NPYDatasetv2(Dataset):
 
         label = row['train']
         return data_tensor, label
+    
+
+class NPYDatasetv2Norm(Dataset):
+    def __init__(self, csv_path):
+        """
+        Args:
+            csv_path (str): Path to the CSV metadata file.
+            base_path (str): Base directory where .npy files are stored.
+            max_samples (int): Maximum number of samples to load.
+            train_only (bool): If True, load only training data; else test data.
+        """
+        # Load CSV
+        df = pd.read_csv(csv_path)
+        #df = df.sample(frac=1).reset_index(drop=True)  # Shuffle data
+
+        self.min= [0, 0, 0, 0, 0, 0, 0, 200, 0, 72, 180, 153, 99, 81, 2461.5, 931.5, 393.3, 3000, 70, 180, 0.45, 0.9, 45, 54, 200, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 200, 0, 72, 180, 153, 99, 81, 2461.5, 931.5, 393.3]
+        self.max= [1, 1, 1, 1, 1, 1, 1, 400, 400, 88, 220, 187, 121, 99, 3709.2, 3037.1, 859.1, 8000, 200, 220, 0.55, 1.1, 55, 66, 400, 400, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 400, 400, 88, 220, 187, 121, 99, 3709.2, 3037.1, 859.1]
+        self.min_vals = np.array(self.min)
+        self.max_vals = np.array(self.max)
+
+        # Store DataFrame
+        self.df = df
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        data = np.load(row['path'])  # shape: (3, 17)
+
+        # Convert numpy array to tensor
+        data_tensor = torch.tensor(data, dtype=torch.float32)
+
+        # Reshape as needed: flatten and add channel dimension
+        data_tensor = data_tensor.reshape(-1)  # flatten to 1D
+
+        # Apply Min–Max normalization
+        data_tensor = (data_tensor - self.min_vals) / (self.max_vals - self.min_vals + 1e-8)  # avoid /0
+        data = np.clip(data, 0.0, 1.0)  # keep clean range
+
+        data_tensor = data_tensor.unsqueeze(0)  # add channel dimension (1, N)
+
+        label = row['train']
+        return data_tensor, label
