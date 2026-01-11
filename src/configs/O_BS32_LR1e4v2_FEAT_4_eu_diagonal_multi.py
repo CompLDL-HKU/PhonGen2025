e550e81c0eb87ecf93b7e46ce30fd62b882d0be4
Frankalexej <tt115889@connect.hku.hk>
2026-01-12 00:19:21 +0800

    20260112 + 1

---
+++ src/configs/O_BS32_LR1e4v2_FEAT_4_eu_diagonal_multi.py
@@ -0,0 +1,7 @@
+# Configuration parameters
+READ_BASE_PATH = '/mnt/storage/ldl_linguistics/PhonGen2025Diagonal/eval_outputs'
+WRITE_BASE_PATH = '/mnt/storage/ldl_linguistics/PhonGen2025Diagonal/eval_outputs'
+RUN_TIMES_START = 1
+RUN_TIMES_END = 11
+RUN_NAME = "NORM_BS32_LR1E4_FEAT4_TEMP007V2_EU_DIAGONAL"
+RUN_NAME_SHORT = "NORM_DIAG"
