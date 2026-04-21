475bb39da37ed2645997469912ccb2922223a26d
Frankalexej <tt115889@connect.hku.hk>
2026-04-21 16:43:18 +0800

    20260421 + 1

---
+++ src/configs/T06_natural_1e4_HID4.py
@@ -0,0 +1,19 @@
+# Configuration parameters
+# Story: 03 is using original variance (std = 5), not using normalization, but changing COG to (0, 40) while FD being still (30, 70). 
+BATCH_SIZE = 32
+EPOCHS = 150
+LR = 1e-4
+DATA_COLLECTION_NAME = 'data_NTNORM'
+DATA_SUFFIX = 'natural'
+CSV_PATH = f'/mnt/storage/ldl_linguistics/PhonGen2025_diag_new/{DATA_COLLECTION_NAME}/data_train_{DATA_SUFFIX}/metadata_train_{DATA_SUFFIX}.csv'
+# CSV_PATH2 = '/mnt/storage/ldl_linguistics/PhonGen2025/data_251011/data_train_phase2_a/metadata_train_phase2_a.csv'
+CSV_PATH3 = f'/mnt/storage/ldl_linguistics/PhonGen2025_diag_new/{DATA_COLLECTION_NAME}/data_test_{DATA_SUFFIX}/metadata_test_{DATA_SUFFIX}.csv'
+DEVICE = 'cuda'
+OUT_FEATURES = 4
+TEMPERATURE = 0.07
+SIMILARITY = "euclidean"
+RUN_NAME = '06_NATURAL_1E4_HID4'
+RUN_TIMES_START = 1
+RUN_TIMES_END = 11
+SAMPLE_LIST = []
+NORMALIZE = False
